d3d2a3ce0d4b6eda55f969e5479532d0ade0cb90
laptop\marcb <marc.berli@windowslive.com>
2017-02-08 21:28:45 +0100

    added typedefinitions

---
+++ src/hal-crawler.d.ts
@@ -0,0 +1,64 @@
+import * as Immutable from 'immutable';
+
+
+export interface HalCrawlerConfigI {
+	root: string,
+	[index: string]: any,
+}
+
+type HalCrawlerConfigMap = Immutable.Map<string, HalCrawlerConfigI>;
+
+export type HalCrawlerAction = 'GET' | 'POST' | 'PUT' | 'DELETE'
+
+
+/**
+* creates and validates a configuration object
+* @param  config object which must contain root url and might contain other config such as http headers e.g.: {root: "localhost:8080/api", headers: {"Accept-Language": "en-US"}}
+* @return configuration instance
+*/
+export function createConfig(config: HalCrawlerConfigI): HalCrawlerConfigMap;
+
+declare class Schema {
+	/**
+   *
+   * @param  string name        will be used as a unique identifier for a schema (there must never be multiple schemas with the same name)
+   * @param  [string] identifiers an array of strings which defines the attributes required to uniquely define a resource
+   * @param  Action action     the action which can be performed on an endpoint
+   * @param  [Schema] children    defines the schemas underneath the parent schema
+   * @return undefined
+   */
+	constructor(name: string, identifiers: string[], action: HalCrawlerAction, children: Schema[])
+
+  getName(): string
+
+  getIdentifiers(): string[]
+
+  getAction(): HalCrawlerAction
+
+  getChildren(): Schema[]
+}
+
+/**
+ * A Resource represents an instance of a Schema definition.
+ */
+declare class Resource {
+  constructor(schema: Schema, link: string, data: any)
+
+  getSchema(): Schema
+
+  getLink(): string
+
+  getData(): any
+
+  isFetched(): boolean
+}
+
+declare class Command {
+  constructor(resource: Resource, action: HalCrawlerAction)
+
+  getResource(): Resource
+
+  getAction(): HalCrawlerAction
+}
+
+export function crawl<T>(config: HalCrawlerConfigMap, store: Immutable.Map<string, T>, resource: Resource, command: Command):Immutable.Map<string, T>;
