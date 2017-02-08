7c55c67569815442a337022d9fba2f053ba88371
Simon Stucki <simon_stucki@hotmail.com>
2017-02-08 22:03:09 +0100

    simplify API

--- src/crawl.js
+++ src/crawl.js
@@ -1,3 +1,4 @@
+import Immutable from "immutable";
 import load from "./utils/load";
 import constants from "./constants";
 import action from "./action";
@@ -5,19 +6,19 @@ import convertToStore from "./utils/convertToStore";
 import getResourceFromStore from "./utils/getResourceFromStore";
 
 /**
- * fetches a HAL resource
+ * fetches a HAL resource and returns an extended version of the store
  * @param  config  [configuration]
- * @param  stores  [current store instance or {}]
- * @param  resource [root resource]
  * @param  command [defines what to execute]
+ * @param  store  [current store instance or {}]
  * @return store with new resources
  */
-export default function crawl(config, store, schema, command) {
+export default function crawl(config, command, store = Immutable.Map({})) {
   const resource = command.getResource();
+  const schema = resource.getSchema();
   const desiredAction = command.getAction();
 
   // for first call to a HAL API there is no resource instance
-  if (resource === undefined) {
+  if (resource.getLink() === undefined) {
     return load(config.get(constants.config.root)).then(response => response.json().then(data => convertToStore(schema, data, store)));
   } else {
     if (desiredAction === action.GET) {
