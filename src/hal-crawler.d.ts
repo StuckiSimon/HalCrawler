46fc7c68fed68c5e28eedee0f04bdcc188931d6e
laptop\marcb <marc.berli@windowslive.com>
2017-02-15 22:55:41 +0100

    typings

--- src/hal-crawler.d.ts
+++ src/hal-crawler.d.ts
@@ -8,8 +8,14 @@ export interface HalCrawlerConfigI {
 
 type HalCrawlerConfigMap = Immutable.Map<string, HalCrawlerConfigI>;
 
-export type HalCrawlerAction = 'GET' | 'POST' | 'PUT' | 'DELETE'
+type HalCrawlerAction = 'GET' | 'POST' | 'PUT' | 'DELETE'
 
+export const action: {
+	GET: 'GET',
+	POST: 'POST',
+	PUT: 'PUT',
+	DELETE: 'DELETE'
+}
 
 /**
 * creates and validates a configuration object
@@ -19,21 +25,21 @@ export type HalCrawlerAction = 'GET' | 'POST' | 'PUT' | 'DELETE'
 export function createConfig(config: HalCrawlerConfigI): HalCrawlerConfigMap;
 
 declare class Schema {
-  /**
+	/**
    *
    * @param  string name        will be used as a unique identifier for a schema (there must never be multiple schemas with the same name)
-   * @param  string[] identifiers an array of strings which defines the attributes required to uniquely define a resource
-   * @param  Action[] actions     the actions which can be performed on an endpoint
-   * @param  Schema[] children    defines the schemas underneath the parent schema
-   * @return instance
+   * @param  [string] identifiers an array of strings which defines the attributes required to uniquely define a resource
+   * @param  Action action     the action which can be performed on an endpoint
+   * @param  [Schema] children    defines the schemas underneath the parent schema
+   * @return undefined
    */
-	constructor(name: string, identifiers: string[], actions: HalCrawlerAction[], children?: Schema[])
+	constructor(name: string, identifiers: string[], action: HalCrawlerAction[], children?: Schema[][])
 
   getName(): string
 
   getIdentifiers(): string[]
 
-  getActiosn(): HalCrawlerAction[]
+  getAction(): HalCrawlerAction
 
   getChildren(): Schema[]
 }
@@ -54,7 +60,7 @@ declare class Resource {
 }
 
 declare class Command {
-  constructor(resource: Resource, action?: HalCrawlerAction, ignoreStore?: boolean)
+  constructor(resource: Resource, action?: HalCrawlerAction)
 
   getResource(): Resource
 
@@ -68,7 +74,7 @@ declare class Command {
  * @param  store  [current store instance or {}]
  * @return store with new resources
  */
-export function crawl<T>(config: HalCrawlerConfigMap, command: Command, store: Immutable.Map<string, T>):Immutable.Map<string, T>;
+export function crawl<T>(config: HalCrawlerConfigMap, command: Command, store: Immutable.Map<string, T>): Promise<T>;
 
 /**
  * loads a given resource from the store
@@ -77,3 +83,10 @@ export function crawl<T>(config: HalCrawlerConfigMap, command: Command, store: I
  * @return Resource          Resource with data
  */
 export function getResourceFromStore<T>(store: Immutable.Map<string, T>, resource: Resource): Resource
+
+/**
+ * creates and validates a configuration object
+ * @param  config object which must contain root url and might contain other config such as http headers e.g.: {root: "localhost:8080/api", headers: {"Accept-Language": "en-US"}}
+ * @return configuration instance
+ */
+export function createConfig(config: { root: string }): Immutable.Map<string, any> 
