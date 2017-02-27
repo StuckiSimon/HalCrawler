3a9be4a11f57ca29e2e109a3b9d70eab14d27711
Simon Stucki <simon_stucki@hotmail.com>
2017-02-27 21:55:22 +0100

    add halux integration example

---
+++ example/halux-integration/src/index.js
@@ -0,0 +1,79 @@
+import Immutable from "immutable";
+import { createConfig, crawl, Command, Schema, Resource, action, getResourceFromStore } from "hal-crawler";
+
+import { createHalux, haluxReducer, createHaluxAction, nestHaluxActions } from 'halux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+
+const client = new Schema("client", ["id"], [action.GET]);
+const clients = new Schema("clients", [], [action.GET], [[client]]);
+const languages = new Schema("languages", [], [action.GET]);
+
+// [] => list of resources, if only one can occur the [] can be removed
+const root = new Schema("root", [], [action.GET], [clients, languages]);
+
+const config = createConfig({
+  root: "rootClientModel.json"
+});
+
+const fetchRoot = () => createHaluxAction({
+    schema: root,
+    identifiers: undefined
+});
+
+const fetchClients = () => createHaluxAction({
+    schema: clients,
+    identifiers: undefined,
+    handlers: {
+        errorHandler: (error) => console.log(error.toString())
+    }
+});
+
+const fetchClient = (clientObject) => createHaluxAction({
+    schema: client,
+    identifiers: {
+      id: clientObject.id
+    },
+    handlers: {
+        errorHandler: (error) => console.log(error.toString())
+    }
+});
+
+const fetchLanguages = () => createHaluxAction({
+    schema: languages,
+    identifiers: undefined
+});
+
+const nestedClients = () => nestHaluxActions(fetchRoot, fetchClients)({}, {});
+const nestedClient = (client) => nestHaluxActions(nestedClients, fetchClient)({}, client);
+const nestedLanguages = () => nestHaluxActions(fetchRoot, fetchLanguages)({}, {});
+
+const trialReducer = (state = {}, action) => {
+  return state;
+}
+
+const reducers = combineReducers({
+    app: {}, // your app state
+    data: combineReducers({
+        halux: haluxReducer,
+        somethingOther: trialReducer,
+    })
+});
+
+const haluxMiddleware = createHalux(config, 'data.halux');
+
+const store = createStore(
+  reducers,
+  undefined,
+  applyMiddleware(haluxMiddleware)
+)
+
+store.dispatch(nestedClient({id: 2}));
+store.dispatch(fetchRoot());
+store.dispatch(nestedLanguages());
+setTimeout(() => {
+  store.dispatch(fetchRoot());
+}, 500);
+
+setTimeout(() => {
+  console.log(store.getState());
+}, 3000);
---
+++ example/halux-integration/webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: __dirname + '/src/index.js',
+  output: {
+    path: __dirname + '/bin',
+    filename: 'app.bundle.js',
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  }
+}
